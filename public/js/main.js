06b6f464ee80c3436fff1e55bbf085d16391bc80
tkvishal <tkvishal@ubuntu.ubuntu-domain>
2019-02-15 21:10:45 +0530

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+var animate = (ele, anim) => {
+  if (ele.classList.contains(anim)) {
+    ele.classList.remove(anim);
+  } else {
+    ele.classList.add(anim);
+    ele.classList.add("animated");
+  }
+};
+
+var animationEnd = (function(el) {
+  var animations = {
+    animation: "animationend",
+    OAnimation: "oAnimationEnd",
+    MozAnimation: "mozAnimationEnd",
+    WebkitAnimation: "webkitAnimationEnd"
+  };
+
+  for (var t in animations) {
+    if (el.style[t] !== undefined) {
+      return animations[t];
+    }
+  }
+})(document.createElement("div"));
+
+body = document.getElementById("main-body");
+window.addEventListener("load", function() {
+  animate(body, "fadeInUp");
+});
